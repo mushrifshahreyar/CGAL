7994a1d528a3fc4af6d7d83e930b18060bcf8914
mushrifshahreyar <mushrifshahreyar@gmail.com>
2020-11-17 00:24:21 +0530

    Q4 openGL

---
+++ Assignment2/Q4/Q4.cpp
@@ -0,0 +1,71 @@
+#include<GL/glut.h> 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+Task to handle:
+    Reshaping of window;
+
+*/
+
+float r,g,b,x,y;
+bool check=true;
+
+void Init() {
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutCreateWindow("Line Drawing");
+    gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+    glClearColor(1, 1, 1, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+}
+
+void mouse(int button, int state, int mousex, int mousey)
+{
+    if(button==GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        check=true;
+        x = mousex;
+        y = 480 - mousey;
+    }
+    else {
+        check = false;
+    }
+            
+    glutPostRedisplay();
+}
+
+void motionfunc(int Mx, int My) {
+    x = Mx;
+    y = 480 - My;
+
+    glutPostRedisplay();
+}
+
+void display(void)
+{  
+    glColor3f(0,0,0);
+    glPointSize(5); 
+    
+    if(check) {
+        glBegin(GL_POINTS);
+        glVertex2i(x,y);
+        glEnd();
+    }
+    glFlush();    
+}
+
+int main(int argc,char** argv)
+{
+	glutInit(&argc,argv);
+    Init();  
+    
+    
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motionfunc);
+    glutMainLoop();
+}
