d8aa10c21978b0d4208e4242869ebd1b2d18154c
mushrifshahreyar <mushrifshahreyar@gmail.com>
2020-11-21 14:04:37 +0530

    Assgn 2 Q4 minor bug fixes

--- Assignment2/Q4/Q4.cpp
+++ Assignment2/Q4/Q4.cpp
@@ -3,31 +3,18 @@
 
 using namespace std;
 
-/*
-Task to handle:
-    Reshaping of window;
-
-*/
-
-float x, y;
-bool check = true;
 int width, height;
-
-void Init() {
-  glutInitWindowSize(640, 480);
-  glutInitWindowPosition(10, 10);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutCreateWindow("Q4 - Mouse Drawing");
-  gluOrtho2D(0.0, 640.0, 0.0, 480.0);
-  glClearColor(1, 1, 1, 0);
-  glClear(GL_COLOR_BUFFER_BIT);
-}
+int x,y;
+bool check;
+int x_new,y_new;
 
 void mouse(int button, int state, int mousex, int mousey) {
   if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
     check = true;
-    x = mousex;
-    y = 480 - mousey;
+    x_new = mousex;
+    y_new = height - mousey;
+    x = x_new;
+    y = y_new;
   } else {
     check = false;
   }
@@ -36,30 +23,64 @@ void mouse(int button, int state, int mousex, int mousey) {
 }
 
 void motionfunc(int Mx, int My) {
-  x = Mx;
-  y = 480 - My;
+  x = x_new;
+  y = y_new;
+  x_new = Mx;
+  y_new = height - My;
 
   glutPostRedisplay();
 }
 
+
 void display(void) {
   glColor3f(0, 0, 0);
-  glPointSize(5);
-
+  glLineWidth(5.0);
   if (check) {
-    glBegin(GL_POINTS);
+    glBegin(GL_LINE_STRIP);
     glVertex2i(x, y);
+    glVertex2i(x_new, y_new);
     glEnd();
   }
   glFlush();
 }
 
-int main(int argc, char **argv) {
+void on_resize(int w, int h)
+{
+
+  width = w;
+  height = h;
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, width, 0.0, height);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+    
+  glClearColor(1,1,1,0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glutPostRedisplay();
+
+}
+
+int main(int argc, char** argv) {
   glutInit(&argc, argv);
-  Init();
+  width = 640;
+  height = 480;
+  //Initialization    
+  glutInitWindowSize(width, height);
+  glutInitWindowPosition(10, 10);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutCreateWindow("Q4 - Mouse Drawing");
+  gluOrtho2D(0.0, width, 0.0, height);
+  glClearColor(1,1,1,0);
+  glClear(GL_COLOR_BUFFER_BIT);
   
+  //Setting functions
   glutDisplayFunc(display);
   glutMouseFunc(mouse);
   glutMotionFunc(motionfunc);
+  glutReshapeFunc(on_resize);
   glutMainLoop();
+  
+  return 0;
 }
